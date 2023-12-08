6f27298df92204f0772be5a7a64bd328d2446f4a
Moritz Niesen <niesenmoritz@gmail.com>
2023-12-08 15:06:56 +0100

    Added Day1 Part1

---
+++ AdventOfCodeCSharp/AdventOfCodeCSharp/Day1Part1.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCodeCSharp;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Day1Part1
+{
+    public static void Run()
+    {
+        var lines = File.ReadAllLines("Day1Part1.txt");
+
+        var totalNum = 0;
+        
+        foreach (var line in lines)
+        {
+            var lineCharArray = line.ToCharArray();
+
+            var lineNum = string.Empty;
+            
+            foreach (var c in lineCharArray)
+            {
+                if (!int.TryParse(c.ToString(), out var num)) continue;
+                lineNum += num;
+                break;
+            }
+
+            for (var i = lineCharArray.Length - 1; i > -1; i--)
+            {
+                var c = lineCharArray[i];
+
+                if (!int.TryParse(c.ToString(), out var num)) continue;
+                lineNum += num;
+                break;
+            }
+
+            totalNum += Convert.ToInt32(lineNum);
+        }
+
+        Console.WriteLine(totalNum);
+    }
+}
---
+++ AdventOfCodeCSharp/AdventOfCodeCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCodeCSharp;
+
+// ReSharper disable once ClassNeverInstantiated.Global
+public class Program
+{
+    public static void Main()
+    {
+        Day1Part1.Run();
+    }
+}
